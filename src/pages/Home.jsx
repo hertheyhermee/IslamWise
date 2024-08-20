2a663615f026332b13f63d20b92a581b5a0c4801
hertheyhermee <tiamiyuhayzed@gmail.com>
2024-08-21 00:14:47 +0100

    first commit

---
+++ src/pages/Home.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const Home = () => {
+  return (
+    <div className='d-flex justify-content-center align-items-center'>
+        <h1 className='text-uppercase fw-5'>Welcome to IslamWise</h1>
+    </div>
+  )
+}
+
+export default Home
