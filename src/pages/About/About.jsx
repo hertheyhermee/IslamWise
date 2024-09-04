9169e58a42e6511eec7fdd1a76f6e26c898e26a6
mukhtaar <tijaanimukhtaar@gmail.com>
2024-09-04 14:29:00 -0700

    about

--- src/pages/About/About.jsx
+++ src/pages/About/About.jsx
@@ -1,9 +1,230 @@
-import React from 'react'
+import React, { useEffect } from "react";
+import bgVideo from "../../assets/bgVid.mp4";
+import couples from "../../assets/couples2.webp";
+import hijab from "../../assets/hijab.webp";
+import AOS from "aos";
+import "aos/dist/aos.css";
+
+import {
+  FaDribbbleSquare,
+  FaFacebookSquare,
+  FaGithubSquare,
+  FaInstagram,
+  FaTwitterSquare,
+} from "react-icons/fa";
 
 const About = () => {
-  return (
-    <div>About</div>
-  )
-}
+  useEffect(() => {
+    AOS.init({ duration: 1000 }); 
+  }, []);
 
-export default About
+  return (
+    <>
+      <div className="mx-auto w-4/5 md:w-full">
+        <h1 className="bg-red-500 text-center">Navbar to be imported here</h1>
+        <div>
+          {/* video */}
+          <video
+            src={bgVideo}
+            autoPlay
+            loop
+            muted
+            playsInline
+            className="w-full h-1/6 object-cover"
+          ></video>
+        </div>
+        {/* hero section */}
+        <div>
+          <h1
+            className="container text-center py-5 font-semibold"
+            data-aos="fade-up" // Add AOS animation
+          >
+            Knowing More About Your Deen
+          </h1>
+        </div>
+        <div
+          className="container flex flex-col lg:flex-row gap-4 items-center justify-center"
+          data-aos="fade-right" // Add AOS animation
+        >
+          <div
+            href="#"
+            className="block max-w-sm p-6 bg-white border border-gray-200 rounded-lg shadow hover:bg-gray-100 dark:bg-gray-800 dark:border-gray-700 dark:hover:bg-gray-700 "
+          >
+            <h5 className="mb-2 text-2xl font-bold tracking-tight text-gray-900 dark:text-white">
+              Noteworthy technology acquisitions 2021
+            </h5>
+            <p className="font-normal text-gray-700 dark:text-gray-400">
+              Here are the biggest enterprise technology acquisitions of 2021 so
+              far, in reverse chronological order.
+            </p>
+          </div>
+          <div
+            href="#"
+            className="block max-w-sm p-6 bg-white border border-gray-200 rounded-lg shadow hover:bg-gray-100 dark:bg-gray-800 dark:border-gray-700 dark:hover:bg-gray-700 "
+          >
+            <h5 className="mb-2 text-2xl font-bold tracking-tight text-gray-900 dark:text-white">
+              Noteworthy technology acquisitions 2021
+            </h5>
+            <p className="font-normal text-gray-700 dark:text-gray-400">
+              Here are the biggest enterprise technology acquisitions of 2021 so
+              far, in reverse chronological order.
+            </p>
+          </div>
+          <div
+            href="#"
+            className="block max-w-sm p-6 bg-white border border-gray-200 rounded-lg shadow hover:bg-gray-100 dark:bg-gray-800 dark:border-gray-700 dark:hover:bg-gray-700 "
+          >
+            <h5 className="mb-2 text-2xl font-bold tracking-tight text-gray-900 dark:text-white">
+              Noteworthy technology acquisitions 2021
+            </h5>
+            <p className="font-normal text-gray-700 dark:text-gray-400">
+              Here are the biggest enterprise technology acquisitions of 2021 so
+              far, in reverse chronological order.
+            </p>
+          </div>
+        </div>
+        {/* how it started */}
+        <div className="container">
+          <h1
+            className="text-center pt-20 font-semibold"
+            data-aos="zoom-in" // Add AOS animation
+          >
+            Islam Wise Is Bringing Deen To Your Doorstep
+          </h1>
+        </div>
+        <div className="container flex flex-col-reverse lg:flex-row my-20 gap-5">
+          <div
+            className="w-full lg:w-1/2 flex items-center justify-center"
+            data-aos="fade-left" // Add AOS animation
+          >
+            <img src={couples} alt="" className="rounded-lg" />
+          </div>
+          <div
+            className="w-full lg:w-1/2"
+            data-aos="fade-right" // Add AOS animation
+          >
+            <h2 className="text-2xl md:text-3xl lg:text-4xl font-bold mb-4 text-center sm:text-left">
+              How it all began
+            </h2>
+            <p className="text-base md:text-lg lg:text-xl text-justify">
+              First of all, we are a family of 5, myself (Hanan), my husband
+              (Abdul Malik), and our 3 children, Jannah, Abdusalaam, and Asiya.
+              Our journey began several years ago when our daughter Jannah began
+              Junior kindergarten. Jannah, a notorious sleepy-head, and despite
+              being very excited to begin school, had a difficult time waking
+              up. My husband, determined to get Jannah up and ready for school
+              pulled out all the stops. He would pick her up and pretend to give
+              her an airplane ride, give her a few extra minutes of shut-eye,
+              and sing songs he would make up. One of the songs happened to be
+              “It’s time for Schule (school- in Kiswahili)”.
+            </p>
+          </div>
+        </div>
+        <div className="container flex flex-col-reverse lg:flex-row my-20 gap-5">
+          <div
+            className="w-full lg:w-1/2"
+            data-aos="fade-right" // Add AOS animation
+          >
+            <h2 className="text-2xl md:text-3xl lg:text-4xl font-bold mb-4 text-center sm:text-left">
+              How it all began
+            </h2>
+            <p className="text-base md:text-lg lg:text-xl text-justify">
+              Jannah's Family and Friends is an animated series based loosely on
+              the lives of our family and Friends. Not only do we produce
+              animated nasheed’s but also books for the hope of inspiring little
+              Muslims and encouraging them to feel proud to be Muslim. We strive
+              to create content that teaches little Muslims and their families
+              the proper perspective of Muslims with regards to the many issues
+              Muslims encounter in the modern world. Also, we hope to educate
+              little Muslims regarding some of the rituals of Islam as well as
+              their relationship with Allah Subhaana wa t’Alaa and our last and
+              final messenger Prophet Muhammad (Peace be upon him). We also
+              provide discussions involving the many issues surrounding raising
+              Muslim children in the modern world. These discussions involve
+              assisting parents on how to confront the many foreign ideologies
+              which creep into the minds of our children. Please subscribe to
+              our Youtube page, and Follow us on Instagram, Facebook, and
+              Tiktok..Soon enough Jannah was inspired to wake up for school and
+              life went on. My husband, not thinking anything of it, carried on
+              singing these songs to Jannah. One day a friend of ours shared a
+              video of a nasheed artist, Ilyas Mao. Malik realized that Ilyas
+              didn’t use instruments and only used his voice for the
+              “beats/music”. It was then that the idea came to Malik to begin to
+              write nasheed’s for Muslim children. After some consultation with
+              a few mashaykh we trusted, we started to embark on the creation of
+              Jannah's Family and Friends. /assets/images/fampic2.jpg
+            </p>
+          </div>
+          <div
+            className="w-full lg:w-1/2 flex items-center justify-center"
+            data-aos="fade-left" // Add AOS animation
+          >
+            <img src={hijab} alt="" className="rounded-lg" />
+          </div>
+          
+        </div>
+        <div className="container my-5">
+        <h2 className="text-center font-semibold text-4xl">Our Mission</h2>
+        <div className="">
+        <p className="text-justify md:text-center text-base md:text-lg lg:text-xl ">
+          As a brand, we are more than just a single feature or application.
+          From prayer times, qibla, content to streaming, our features have
+          distinct propositions that add value to Muslim life. Since 2010, we
+          have pioneered the Muslim tech scene and built a digital home where
+          Muslims belong, whoever and wherever they are. Our pioneering platform
+          nurtures and enriches everyday experiences by collaborating with
+          trailblazing Muslims to champion and discover the latest trends,
+          moments, and ideas. We are diverse a bunch of individuals of different
+          nationalities across geographic regions. We are part of the Muslim
+          community and are committed to the cause of progressing Muslim life.
+        </p>
+        </div>
+        </div>
+
+      </div>
+      {/* footer */}
+      <div className="container mx-auto w-4/5 md:w-full py-16 px-4 grid lg:grid-cols-3 gap-8 text-gray-700">
+        <div>
+          <div className="flex justify-between md:w-[75%] my-6">
+            <FaFacebookSquare size={30} />
+            <FaInstagram size={30} />
+            <FaTwitterSquare size={30} />
+            <FaGithubSquare size={30} />
+            <FaDribbbleSquare size={30} />
+          </div>
+        </div>
+        <div className="lg:col-span-3 flex justify-between mt-6">
+          <div>
+            <h2 className="font-medium text-gray-400">Solutions</h2>
+            <ul>
+              <li className="py-2 text-sm">Inheritance</li>
+              <li className="py-2 text-sm">Fiqh</li>
+              <li className="py-2 text-sm">Quran</li>
+              <li className="py-2 text-sm">Hadeeth</li>
+            </ul>
+          </div>
+          <div>
+            <h2 className="font-medium text-gray-400">Support</h2>
+            <ul>
+              <li className="py-2 text-sm">Pricing</li>
+              <li className="py-2 text-sm">Documentation</li>
+              <li className="py-2 text-sm">Guides</li>
+              <li className="py-2 text-sm">API Status</li>
+            </ul>
+          </div>
+
+          <div>
+            <h2 className="font-medium text-gray-400">Legal</h2>
+            <ul>
+              <li className="py-2 text-sm">Claim</li>
+              <li className="py-2 text-sm">Policy</li>
+              <li className="py-2 text-sm">Terms</li>
+            </ul>
+          </div>
+        </div>
+      </div>
+    </>
+  );
+};
+
+export default About;
--- src/pages/InheritanceBlog/InheritanceBlog.jsx
+++ src/pages/InheritanceBlog/InheritanceBlog.jsx
@@ -1,9 +1,97 @@
-import React from 'react'
+import React from "react";
+import { useEffect } from "react";
+import inheritance1 from "../../assets/inheritance3.webp";
+import inheritance2 from "../../assets/inheritance2.webp";
+import AOS from "aos";
+import "aos/dist/aos.css";
 
 const InheritanceBlog = () => {
-  return (
-    <div>InheritanceBlog</div>
-  )
-}
+  useEffect(() => {
+    AOS.init({ duration: 1000 });
+  }, []);
 
-export default InheritanceBlog
+  return (
+    <>
+      <div className="bg-red-500 text-center">navbar should here</div>
+      <div className="container mx-auto p-4 grid grid-cols-1 md:grid-cols-2 lg:grid-cols-5 gap-y-16 lg:gap-x-24 text-lg items-center">
+        <div className="lg:col-span-3">
+          <h1 className="text-3xl md:text-4xl mb-8 lg:mb-10 text-accent-400 font-medium">
+            Welcome to The Islamic Inheritance Blog
+          </h1>
+
+          <div className="space-y-6 md:space-y-8">
+            <p>
+              In the Islamic tradition, the rules around the distribution of
+              wealth are carefully outlined in the Shariah and apply to every
+              Muslim. These rules are strict and do not differ from person to
+              person, which is the primary difference between an Islamic will
+              and a conventional will.{" "}
+            </p>
+            <p>
+              While the fixed shares of one’s wealth are outlined in the
+              Shariah, there are a number of conditions that must be met in each
+              case. Bequeathing or ‘gifting’ is also allowed in an Islamic will,
+              given that certain conditions are also met.
+            </p>
+            <p>
+              It can be tricky to know where to start when compiling an Islamic
+              will, since the matter is rich in rules and guidelines. Seeking
+              professional advice, services and support is crucial for ensuring
+              that your will is valid.
+            </p>
+          </div>
+        </div>
+
+        <div className="lg:col-span-2">
+          <img
+            src={inheritance1}
+            alt="Family sitting around a fire pit in front of cabin"
+            className="w-full h-auto object-cover rounded-md"
+          />
+        </div>
+
+        <div className="lg:col-span-2">
+          <img
+            src={inheritance2}
+            alt="Family that manages The Wild Oasis"
+            className="w-full h-auto object-cover rounded-md"
+          />
+        </div>
+
+        <div className="lg:col-span-3">
+          <h1 className="text-3xl md:text-4xl mb-8 lg:mb-10 text-accent-400 font-medium">
+            Learning The Islamic Inheritance Through Blogz
+          </h1>
+
+          <div className="space-y-6 md:space-y-8">
+            <p>
+              In Islam, the heirs of a deceased Muslim’s wealth are determined
+              in the Shariah and apply to every Muslim. What’s more, a Muslim is
+              not permitted to distribute their wealth to only a select few of
+              the heirs that are outlined, the heirs and their shares are fixed.
+            </p>
+            <p>
+              In Islam, a wife is entitled to a quarter share of her husband’s
+              estate upon his passing if she has no children. In the instance
+              that she does have children, she is only entitled to one eighth.
+              If the wife passes, the husband will receive half of the deceased
+              wife’s estate if she has no children, and a quarter share if she
+              does.{" "}
+            </p>
+
+            <div>
+              <a
+                href="https://athaqafahaorg.substack.com/p/medical-forensic-evidence-and-its"
+                className="inline-block mt-4 bg-accent-500 px-6 py-4 text-primary-800 text-base md:text-lg font-semibold hover:bg-accent-600 transition-all"
+              >
+                Explore More on inheritance
+              </a>
+            </div>
+          </div>
+        </div>
+      </div>
+    </>
+  );
+};
+
+export default InheritanceBlog;
